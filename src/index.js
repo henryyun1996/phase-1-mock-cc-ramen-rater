079dc3d8bb9773646c247622661f4dcceda82dcc
Henry <henrysungyun@gmail.com>
2023-02-08 10:31:23 -0800

    oof

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 // write your code here
+const API = "http://localhost:3000/ramens"
+const ramenPics = document.querySelector('#ramen-menu');
+const ramenData = document.querySelector('#ramen-detail');
+
+function renderImages(ramen) {
+    const ramenInfo = document.createElement('img');
+    ramenInfo.src = ramen.image;
+    ramenPics.append(ramenInfo);
+}
+
+// GET fetch response
+fetch(API)
+    .then(response => response.json())
+    .then(ramen => ramen.forEach(renderImages));
+
+ramenPics.addEventListener('click', showInfo);
+
+function showInfo(e) {
+    console.log('click'); 
+    document.getElementById
+
+
+}
