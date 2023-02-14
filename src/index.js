c4a5797a1faa9e2a6a1f512f01e4c88ec7411f0e
Henry <henrysungyun@gmail.com>
2023-02-14 14:33:03 -0800

    updated/ complete

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
-// write your code here
-const API = "http://localhost:3000/ramens"
-const ramenPics = document.querySelector('#ramen-menu');
-const ramenData = document.querySelector('#ramen-detail');
+const API = "http://localhost:3000/ramens";
 
-function renderImages(ramen) {
-    const ramenInfo = document.createElement('img');
-    ramenInfo.src = ramen.image;
-    ramenPics.append(ramenInfo);
-}
-
-// GET fetch response
 fetch(API)
     .then(response => response.json())
-    .then(ramen => ramen.forEach(renderImages));
+    .then(ramenImage => ramenImage.forEach(renderImages));
 
-ramenPics.addEventListener('click', showInfo);
+function renderImages(ramen) {
+    // console.log(ramen.image);
+    const menu = document.querySelector('#ramen-menu');
+    const img = document.createElement('img');
+    img.src = ramen.image;
+    menu.append(img);
 
-function showInfo(e) {
-    console.log('click'); 
-    document.getElementById
+    img.addEventListener('click', () => {
+        console.log(ramen);
+        document.querySelector('.detail-image').src = ramen.image;
+        document.querySelector('.detail-image').alt = ramen.name;
+        document.querySelector('.name').innerText = ramen.name;
+        document.querySelector('.restaurant').innerText = ramen.restaurant;
+        document.querySelector('#comment-display').innerText = ramen.comment;
+        document.querySelector('#rating-display').innerText = ramen.rating;
+    })
+}
 
+const newRamen = document.querySelector('#new-ramen');
+newRamen.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let ramenObj = {
+        name: e.target['name'].value,
+        restaurant: e.target['restaurant'].value,
+        image: e.target['image'].value,
+        rating: e.target['rating'].value,
+        comment: e.target['new-comment'].value
+    }
+    renderImages(ramenObj);
+    confirmRamen(ramenObj);
+})
 
+function confirmRamen(ram) {
+    fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application.json",
+            Accept: "application.json"
+        },
+        body: JSON.stringify(ram)
+    })
+    .then(response => response.json());
 }
